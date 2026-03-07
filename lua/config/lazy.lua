c9e04a0fa298e979f247d17f2f49172d76c114f9
aatbip <anantabipal2010@gmail.com>
2026-03-07 13:39:51 +0545

    LazyVim update checker frequency updated

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -28,5 +28,8 @@ require("lazy").setup({
 	-- colorscheme that will be used when installing plugins.
 	-- install = { colorscheme = { "habamax" } },
 	-- automatically check for plugin updates
-	checker = { enabled = true },
+	checker = {
+		enabled = true,
+		frequency = 2592000, -- update check frequency in seconds, 1 month set now
+	},
 })
