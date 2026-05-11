bca5fd1c6f6b9f4e85866236e043eb06cd1eff26
aatbip <anantabipal2010@gmail.com>
2026-05-11 22:31:46 +0545

    fix

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -4,7 +4,20 @@ return {
 	dependencies = { "nvim-lua/plenary.nvim", "BurntSushi/ripgrep" },
 	config = function()
 		local telescope = require("telescope")
-		telescope.setup({})
+		telescope.setup({
+			defaults = {
+				vimgrep_arguments = {
+					"rg",
+					"--color=never",
+					"--no-heading",
+					"--with-filename",
+					"--line-number",
+					"--column",
+					"--smart-case",
+					"--no-ignore",
+				},
+			},
+		})
 		-- Custom highlight overrides (updated for dark, subdued colors with good visibility)
 		vim.api.nvim_set_hl(0, "TelescopeSelection", { bg = "#3e4452" })
 		vim.api.nvim_set_hl(0, "TelescopeMatching", { fg = "#98c379", bold = true })
@@ -12,7 +25,9 @@ return {
 		vim.api.nvim_set_hl(0, "TelescopePreviewLine", { bg = "#3e4452" })
 		vim.api.nvim_set_hl(0, "TelescopeBorder", { fg = "#4b5263" })
 		local builtin = require("telescope.builtin")
-		vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
+		vim.keymap.set("n", "<leader>ff", function()
+			builtin.find_files({ no_ignore = true, hidden = true })
+		end, { desc = "Telescope find files" })
 		vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
 		vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
 		vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
